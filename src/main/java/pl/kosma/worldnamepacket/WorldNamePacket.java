47d4930661afdbfa7d76e1b11663dc493c8971d5
granis <granis@gmail.com>
2022-07-04 17:52:17 +0200

    feat: prefix bungee-worlds with their server-name

--- src/main/java/pl/kosma/worldnamepacket/WorldNamePacket.java
+++ src/main/java/pl/kosma/worldnamepacket/WorldNamePacket.java
@@ -3,5 +3,6 @@ package pl.kosma.worldnamepacket;
 public class WorldNamePacket {
     public static final byte PACKET_ID = 0;
     public static final String CHANNEL_NAME_VOXELMAP = "worldinfo:world_id";
-    public static final String CHANNEL_NAME_XAEROMAP = "xaeroworldmap:main";			
+    public static final String CHANNEL_NAME_XAEROMAP = "xaeroworldmap:main";
+    public static final String CHANNEL_NAME_BUNGEECORD = "BungeeCord";
 }
